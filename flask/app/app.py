80fceb7794a811bd00fb0333e9883a170be7dc3e
conradolega <conradolega@gmail.com>
2016-12-28 08:57:43 +0800

    Add simple retry functionality for app

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,15 +1,29 @@
 import psycopg2
+import sys
+import time
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-conn = psycopg2.connect(
-    host='db',
-    database='app',
-    user='app',
-    password='password',
-)
-cur = conn.cursor()
+retries = 0
+cur = None
+while not cur and retries < 5:
+    print('Connecting to database...')
+    try:
+        conn = psycopg2.connect(
+            host='db',
+            database='app',
+            user='app',
+            password='password',
+        )
+        cur = conn.cursor()
+    except psycopg2.OperationalError as e:
+        retries += 1
+        sleep(5)
+
+if not cur:
+    print('Unable to connect to database after 5 retries')
+    exit(1)
 
 result = cur.execute('''
     SELECT EXISTS(
